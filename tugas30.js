65d38aa753c16e6cc405b3fa32b9ea9a79532ed7
bima rijaluddin <bimarijal@gmail.com>
2019-09-20 14:32:43 +0800

    Tugas 30 Javascript
    Tugas dari niomic

---
+++ tugas30.js
@@ -0,0 +1,7 @@
+function pRegex(value){
+    var data = value.match(/[es]/g);
+    console.log(data);
+}
+
+pRegex( "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
+   
